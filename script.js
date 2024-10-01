d661935349463650f20d648ac249152b2acf07b0
ashitaka3948 <[email protected>
2024-10-01 16:15:24 +0800

    made button to set size

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
+let chosenLength = 16;
+
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-  const horizontalDiv = document.createElement("div");
-  horizontalDiv.className = "horizontal";
-  container.appendChild(horizontalDiv);
+drawGrid(chosenLength);
 
-  for (let x = 0; x < 16; x++) {
-    const verticalDiv = document.createElement("div");
-    verticalDiv.className = "vertical";
-    horizontalDiv.appendChild(verticalDiv);
+const button = document.querySelector("button");
+button.addEventListener("click", () => setLength());
+
+function setLength() {
+  const userInput = prompt("Enter the length of the grid:");
+  if (userInput > 100) {
+    alert("The length cannot exceed 100.");
+  } else if (userInput < 1) {
+    alert("The length cannot be less than 1");
+  } else {
+    chosenLength = userInput;
+    clearGrid();
+    drawGrid(chosenLength);
   }
 }
 
-const pixels = document.querySelectorAll(".vertical");
-pixels.forEach(pixel => {
-  pixel.addEventListener('mouseover', function(event) {
-    event.target.style.backgroundColor = "red";
+function drawGrid(length) {
+
+  for (let i = 0; i < length; i++) {
+    const horizontalDiv = document.createElement("div");
+    horizontalDiv.className = "horizontal";
+    container.appendChild(horizontalDiv);
+
+    for (let x = 0; x < length; x++) {
+      const verticalDiv = document.createElement("div");
+      verticalDiv.className = "vertical";
+      horizontalDiv.appendChild(verticalDiv);
+    }
+  }
+
+  const pixels = document.querySelectorAll(".vertical");
+  pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', function(event) {
+      event.target.style.backgroundColor = "red";
+    });
   });
-});
+}
+
+function clearGrid() {
+  container.innerHTML = "";
+}
